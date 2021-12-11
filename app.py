cf9a75717e30c2aeb35946a3ee336e5b08dd9716
meryyulinda <meryrahmi@gmail.com>
2021-12-11 20:08:57 +0700

    deployment pertama

--- app.py
+++ app.py
@@ -1,40 +1,49 @@
 from flask import Flask, request, render_template
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 
-model_file = open('model.pkl', 'rb')
+model_file = open('DecisionTreemodel.sav', 'rb')
 model = pickle.load(model_file, encoding='bytes')
 
 @app.route('/')
 def index():
-    return render_template('index.html', insurance_cost=0)
+    return render_template('index.html', chanceOfAdmit=0)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     '''
-    Predict the insurance cost based on user inputs
-    and render the result to the html page
+    Predict the admission chance based on 
+    GRE Scores ( out of 340 )
+    TOEFL Scores ( out of 120 )
+    University Rating ( out of 5 )
+    Statement of Purpose and Letter of Recommendation Strength ( out of 5 )
+    Undergraduate GPA ( out of 10 )
+    Research Experience ( either 0 or 1 )
     '''
-    age, sex, smoker = [x for x in request.form.values()]
+    GRE, TOEFL, UnivRating, SOP, LOR, CGPA, Research = [x for x in request.form.values()]
 
     data = []
 
-    data.append(int(age))
-    if sex == 'Laki-laki':
-        data.extend([0, 1])
-    else:
-        data.extend([1, 0])
+    data.append(int(GRE))
+    data.append(int(TOEFL))
+    data.append(int(UnivRating))
+    data.append(float(SOP))
+    data.append(float(LOR))
+    data.append(float(CGPA))
 
-    if smoker == 'Ya':
-        data.extend([0, 1])
+    if Research == "Yes":
+        data.append(1)
     else:
-        data.extend([1, 0])
+        data.append(0)
+
     
     prediction = model.predict([data])
-    output = round(prediction[0], 2)
+    output = prediction[0]
 
-    return render_template('index.html', insurance_cost=output, age=age, sex=sex, smoker=smoker)
+    return render_template('result.html', chanceOfAdmit=output, GRE=GRE, TOEFL=TOEFL, UnivRating=UnivRating, 
+    SOP=SOP, LOR=LOR, CGPA=CGPA, Research=Research)
 
 
 if __name__ == '__main__':
